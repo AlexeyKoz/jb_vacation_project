6df8d0926c99bd37002eb630737f006022dc7718
Doron <doron1405@gmail.com>
2025-03-05 20:38:02 +0200

    email test user_dal

--- src/dal/user_dal.py
+++ src/dal/user_dal.py
@@ -59,4 +59,12 @@ class UserDAL:
                     self.db.rollback()
                     return None
 
+    def init(self, db: Session):
+        self.db = db
 
+    def check_email_exists(self, email: str):
+        try:
+            return self.db.query(User).filter(User.email == email).first() is not None
+        except Exception as e:
+            print(f"Error checking email: {e}")
+            return False
