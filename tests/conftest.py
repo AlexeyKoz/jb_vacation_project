4a9f78e59d9594ca2e2adac78b0f22d80e3cb1e4
Alexey Kozlov <99757449+AlexeyKoz@users.noreply.github.com>
2025-03-13 20:47:31 +0200

    Tests_added_with_bugs3

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,11 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from base import Base
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 
 
