af5e5226f548943f10601c1cbe4e2a0bf25aadba
Doron <doron1405@gmail.com>
2025-03-16 18:07:03 +0200

    update project

--- tests/test_user_facade.py
+++ tests/test_user_facade.py
@@ -1,110 +1,110 @@
-# from src.services.user_service import UserService
-# from conftest import db_session
-# from sqlalchemy.orm import Session
-# from src.services.country_service import CountryService
-# from src.services.like_service import LikeService
-# from src.services.vacation_service import VacationService
-#
-#
-#
-# def test_user_registration_success(db_session):
-#     user_service = UserService(db_session)
-#     user = user_service.create_user("ben", "gvir", "bengvir@gmail.com", "asds", 2)
-#
-#     try:
-#         assert user is not None
-#         assert isinstance(user.email, str)
-#     finally:
-#         db_session.delete(user)
-#         db_session.commit()
-#
-#
-# def test_user_registration_failed(db_session):
-#     user_service = UserService(db_session)
-#     user = user_service.create_user("asd", "gvir", 123123 , "1234", 2)
-#
-#     try:
-#         assert user.email is str
-#         assert user is not None
-#
-#     finally:
-#         db_session.delete(user)
-#         db_session.commit()
-#
-#
-#
-# def test_login_positive(db_session):
-#     user_service = UserService(db_session)
-#     user = user_service.create_user("Bibi", "Netanyahu", "bibiNetanyahu@gmail.com","1234",role_id=2)
-#     login = user_service.login("bibiNetanyahu@gmail.com", "1234")
-#     db_session.commit()
-#
-#     try:
-#         assert login is not None
-#         assert login.email == "bibiNetanyahu@gmail.com"
-#         assert login.password == "1234"
-#
-#     finally:
-#         db_session.delete(user)
-#         db_session.commit()
-#
-# def test_login_negative(db_session):
-#     user_service = UserService(db_session)
-#     user = user_service.create_user("bibi", "Netanyahu", "bibiNetanyahu@gmail.com", "1234", role_id=2)
-#
-#     try:
-#         login = user_service.login("", "1234")
-#         assert login.email is not None
-#
-#     finally:
-#         db_session.delete(user)
-#         db_session.commit()
-#
-#
-#
-# def test_like_vacation_positive(db_session):
-#     user_service = UserService(db_session)
-#     country_service = CountryService(db_session)
-#     vacation_service = VacationService(db_session)
-#     like_service = LikeService(db_session)
-#
-#     user = user_service.create_user("Nasrin", "Kadri", "nasrinKadri@gmai.com", "1234", 2)
-#     country = country_service.create_country("Japan")
-#     country_id = country.id
-#     vacation = vacation_service.create_vacation(country_id, "Japan Trip", "2025-07-01", "2025-07-10", 1700.00, "japan.jpg")
-#
-#     try:
-#         like = like_service.add_like(user.id, vacation.id)
-#         assert like is not None
-#         unlike = like_service.remove_like(user.id, vacation.id)
-#         assert unlike is True
-#
-#     finally:
-#         db_session.delete(user)
-#         db_session.delete(country)
-#         db_session.delete(vacation)
-#         db_session.commit()
-#
-# def test_like_vacation_negative(db_session):
-#     user_service = UserService(db_session)
-#     country_service = CountryService(db_session)
-#     vacation_service = VacationService(db_session)
-#     like_service = LikeService(db_session)
-#
-#     user = user_service.create_user("Nasrin", "Kadri", "nasrinKadri@gmai.com", "1234", 2)
-#     country = country_service.create_country("Japan")
-#     country_id = country.id
-#     vacation = vacation_service.create_vacation(country_id, "Japan Trip", "2025-07-01", "2025-07-10", 1700.00, "japan.jpg")
-#
-#     try:
-#         like = like_service.add_like("domines", vacation.id)
-#         assert like is not None
-#         unlike = like_service.remove_like(user.id, vacation.id)
-#         assert unlike is True
-#
-#     finally:
-#         db_session.delete(user)
-#         db_session.delete(country)
-#         db_session.delete(vacation)
-#         db_session.commit()
+from src.services.user_service import UserService
+from conftest import db_session
+from sqlalchemy.orm import Session
+from src.services.country_service import CountryService
+from src.services.like_service import LikeService
+from src.services.vacation_service import VacationService
+
+
+
+def test_user_registration_success(db_session):
+    user_service = UserService(db_session)
+    user = user_service.create_user("ben", "gvir", "bengvir@gmail.com", "asds", 2)
+
+    try:
+        assert user is not None
+        assert isinstance(user.email, str)
+    finally:
+        db_session.delete(user)
+        db_session.commit()
+
+
+def test_user_registration_failed(db_session):
+    user_service = UserService(db_session)
+    user = user_service.create_user("asd", "gvir", 123123 , "1234", 2)
+
+    try:
+        assert user.email is str
+        assert user is not None
+
+    finally:
+        db_session.delete(user)
+        db_session.commit()
+
+
+
+def test_login_positive(db_session):
+    user_service = UserService(db_session)
+    user = user_service.create_user("Bibi", "Netanyahu", "bibiNetanyahu@gmail.com","1234",role_id=2)
+    login = user_service.login("bibiNetanyahu@gmail.com", "1234")
+    db_session.commit()
+
+    try:
+        assert login is not None
+        assert login.email == "bibiNetanyahu@gmail.com"
+        assert login.password == "1234"
+
+    finally:
+        db_session.delete(user)
+        db_session.commit()
+
+def test_login_negative(db_session):
+    user_service = UserService(db_session)
+    user = user_service.create_user("bibi", "Netanyahu", "bibiNetanyahu@gmail.com", "1234", role_id=2)
+
+    try:
+        login = user_service.login("", "1234")
+        assert login.email is not None
+
+    finally:
+        db_session.delete(user)
+        db_session.commit()
+
+
+
+def test_like_vacation_positive(db_session):
+    user_service = UserService(db_session)
+    country_service = CountryService(db_session)
+    vacation_service = VacationService(db_session)
+    like_service = LikeService(db_session)
+
+    user = user_service.create_user("Nasrin", "Kadri", "nasrinKadri@gmai.com", "1234", 2)
+    country = country_service.create_country("Japan")
+    country_id = country.id
+    vacation = vacation_service.create_vacation(country_id, "Japan Trip", "2025-07-01", "2025-07-10", 1700.00, "japan.jpg")
+
+    try:
+        like = like_service.add_like(user.id, vacation.id)
+        assert like is not None
+        unlike = like_service.remove_like(user.id, vacation.id)
+        assert unlike is True
+
+    finally:
+        db_session.delete(user)
+        db_session.delete(country)
+        db_session.delete(vacation)
+        db_session.commit()
+
+def test_like_vacation_negative(db_session):
+    user_service = UserService(db_session)
+    country_service = CountryService(db_session)
+    vacation_service = VacationService(db_session)
+    like_service = LikeService(db_session)
+
+    user = user_service.create_user("Nasrin", "Kadri", "nasrinKadri@gmai.com", "1234", 2)
+    country = country_service.create_country("Japan")
+    country_id = country.id
+    vacation = vacation_service.create_vacation(country_id, "Japan Trip", "2025-07-01", "2025-07-10", 1700.00, "japan.jpg")
+
+    try:
+        like = like_service.add_like("domines", vacation.id)
+        assert like is not None
+        unlike = like_service.remove_like(user.id, vacation.id)
+        assert unlike is True
+
+    finally:
+        db_session.delete(user)
+        db_session.delete(country)
+        db_session.delete(vacation)
+        db_session.commit()
 
--- tests/test_vacation_facade.py
+++ tests/test_vacation_facade.py
@@ -1,36 +1,36 @@
 from src.services.vacation_service import VacationService
 from src.services.country_service import CountryService
 
-# def test_create_vacation_positive(db_session):
-#     vacation_service = VacationService(db_session)
-#     country_service = CountryService(db_session)
-#     country = country_service.create_country("Japan")
-#     country_id = country.id
-#
-#     try:
-#         vacation = vacation_service.create_vacation(country_id, "Trip to Bali", "2025-06-01", "2025-06-10", 1500.00,"bali.jpg")
-#         assert vacation is not None
-#         assert vacation.description == "Trip to Bali"
-#
-#     finally:
-#         db_session.delete(country)
-#         db_session.commit()
-#
-#
-# def test_create_vacation_negative(db_session):
-#     vacation_service = VacationService(db_session)
-#     country_service = CountryService(db_session)
-#     country = country_service.create_country("Japan")
-#     country_id = country.id
-#
-#     try:
-#         vacation = vacation_service.create_vacation(country_id, "Trip to Bali", "2025-06-01", "2025-05-10", 1500.00,"bali.jpg")
-#         assert vacation is not None
-#         assert vacation.description == "Trip to Bali"
-#
-#     finally:
-#         db_session.delete(country)
-#         db_session.commit()
+def test_create_vacation_positive(db_session):
+    vacation_service = VacationService(db_session)
+    country_service = CountryService(db_session)
+    country = country_service.create_country("Japan")
+    country_id = country.id
+
+    try:
+        vacation = vacation_service.create_vacation(country_id, "Trip to Bali", "2025-06-01", "2025-06-10", 1500.00,"bali.jpg")
+        assert vacation is not None
+        assert vacation.description == "Trip to Bali"
+
+    finally:
+        db_session.delete(country)
+        db_session.commit()
+
+
+def test_create_vacation_negative(db_session):
+    vacation_service = VacationService(db_session)
+    country_service = CountryService(db_session)
+    country = country_service.create_country("Japan")
+    country_id = country.id
+
+    try:
+        vacation = vacation_service.create_vacation(country_id, "Trip to Bali", "2025-06-01", "2025-05-10", 1500.00,"bali.jpg")
+        assert vacation is not None
+        assert vacation.description == "Trip to Bali"
+
+    finally:
+        db_session.delete(country)
+        db_session.commit()
 
 def test_get_all_vacations_positive(db_session):
     vacation_service = VacationService(db_session)
@@ -59,7 +59,7 @@ def test_get_all_vacations_negative(db_session):
     country_id = country.id
     country_id2 = country2.id
     vacation_service.create_vacation(country_id, "Tokyo Trip", "2025-08-12", "2025-08-22", 4000.00, "Tokyo.jpg")
-    vacation_service.create_vacation(country_id2, "Oslo Trip", "2025-08-01", "2025-08-15", 2000.00, "Oslo.jpg")
+    vacation_service.create_vacation(country_id2, "Oslo Trip", "2025-08-01", "2025-08-15", 20000.00, "Oslo.jpg")
     vacations = vacation_service.get_all_vacations()
 
     try:
